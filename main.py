32f5abbfcb150ee09125bbc107ad0881f5011278
unknown <little_black@sjtu.edu.cn>
2023-05-09 10:39:49 +0800

    basic version constructed

---
+++ main.py
@@ -0,0 +1,16 @@
+import utils
+from Data_Collector import DataCollector
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--cfg_file', type=str, default='.\\config\\simple_config.yaml', help='specify the config for collector')
+
+    args = parser.parse_args()
+    collector_config = utils.config_from_yaml(args.cfg_file)
+
+    data_collector = DataCollector(collector_config)
+    data_collector.start_collecting()
+
+if __name__ == '__main__':
+    main()
